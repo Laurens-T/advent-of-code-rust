b53f26703a2dfbf4a9e286a2fad39034e4e8e903
Laurens Teirlynck <laurens.teirlynck@protonmail.com>
2024-12-01 14:16:03 +0100

    rename some stuff

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,18 +3,18 @@ use std::iter::zip;
 
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (mut first, mut second) = parse_lines(input);
+pub fn part_one(input: &str) -> Option<i32> {
+    let (mut col1, mut col2) = parse_lines(input);
 
-    first.sort();
-    second.sort();
+    col1.sort();
+    col2.sort();
 
     let mut result = 0;
-    for (a, b) in zip(first, second) {
+    for (a, b) in zip(col1, col2) {
         result += (a - b).abs();
     }
 
-    Some(result as u32)
+    Some(result)
 }
 
 fn parse_lines(input: &str) -> (Vec<i32>, Vec<i32>) {
@@ -24,29 +24,30 @@ fn parse_lines(input: &str) -> (Vec<i32>, Vec<i32>) {
         .map(|line| line.split("   ").collect())
         .collect();
 
-    let first: Vec<i32> = ids.iter().map(|s| s[0].parse::<i32>().unwrap()).collect();
-    let second: Vec<i32> = ids.iter().map(|s| s[1].parse::<i32>().unwrap()).collect();
+    let col1: Vec<i32> = ids.iter().map(|s| s[0].parse::<i32>().unwrap()).collect();
+    let col2: Vec<i32> = ids.iter().map(|s| s[1].parse::<i32>().unwrap()).collect();
 
-    (first, second)
+    (col1, col2)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (first, second) = parse_lines(input);
+pub fn part_two(input: &str) -> Option<i32> {
+    let (col1, col2) = parse_lines(input);
 
     let mut counts = HashMap::new();
-    second.into_iter().for_each(|v| {
+    col2.into_iter().for_each(|v| {
         counts.entry(v).and_modify(|c| *c += 1).or_insert(1);
     });
 
     let mut result = 0;
-    for val in first {
-        result += val * match counts.get(&val) {
-            None => 0,
-            Some(&count) => count,
-        };
+    for val in col1 {
+        result += val
+            * match counts.get(&val) {
+                None => 0,
+                Some(&count) => count,
+            };
     }
 
-    Some(result as u32)
+    Some(result)
 }
 
 #[cfg(test)]
