20bf85a74a41cd9074a73c84ba7cd26d400fa13c
Laurens Teirlynck <laurens.teirlynck@protonmail.com>
2024-12-02 20:35:53 +0100

    Day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,76 @@
+use std::iter::zip;
+
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = parse_input(input);
+    Some(grid.into_iter().filter(|report| is_safe(report)).count() as u32)
+}
+
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|val| val.parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let (mut incr, mut decr) = (0, 0);
+
+    for (a, b) in zip(report, report.iter().skip(1)) {
+        if a > b {
+            decr += 1;
+        } else {
+            incr += 1;
+        }
+    }
+
+    if incr != 0 && decr != 0 {
+        return false;
+    }
+
+    zip(report, report.iter().skip(1)).all(|(a, b)| (1..=3).contains(&(a - b).abs()))
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let grid = parse_input(input);
+    Some(
+        grid.into_iter()
+            .filter(|report| {
+                for i in 0..report.len() {
+                    // bruteforce
+                    // rust is blazingly fast 👌
+                    let mut cp = report.clone();
+                    cp.remove(i);
+
+                    if is_safe(&cp) {
+                        return true;
+                    }
+                }
+
+                is_safe(report)
+            })
+            .count() as u32,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(5));
+    }
+}
